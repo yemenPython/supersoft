a6d914b6e15d5d57ca5075c09b0bc29bda8554ad
mosa_mosa <eslamslame35@gmail.com>
2021-08-15 21:13:38 +0200

    show store quantity

--- resources/views/admin/concessions/show_part_quantity.blade.php
+++ resources/views/admin/concessions/show_part_quantity.blade.php
@@ -1,6 +1,21 @@
-@foreach($part->stores as $store)
+<table id="sale_supply_table" class="table table-bordered" style="width:100%">
+    <thead>
     <tr>
-        <td scope="col">{{$store->name}}</td>
-        <td scope="col">{{$store->pivot ? $store->pivot->quantity : -0 }}</td>
+        <th scope="col">{!! __('Store name.') !!}</th>
+        <th scope="col">{!! __('Quantity') !!}</th>
     </tr>
-@endforeach
+    </thead>
+
+    <tbody id="part_quantity">
+
+        @foreach($part->stores as $store)
+            <tr>
+                <td >{{$store->name}}</td>
+                <td >{{$store->pivot ? $store->pivot->quantity : 0 }}</td>
+            </tr>
+        @endforeach
+
+
+    </tbody>
+
+</table>
