bd83ee300bac67f6637bea89698321e470d28c3c
DESKTOP-QHDL5Q6\Lenovo <hamdiantar27@gmail.com>
2021-09-05 23:01:01 +0200

    save

---
+++ app/Observers/BranchObserver.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Banks\TypeBankAccount;
+use App\Models\Branch;
+
+class BranchObserver
+{
+    public function created(Branch $branch)
+    {
+        TypeBankAccount::create([
+            'branch_id' => $branch->id,
+            'name_ar' => 'حسابات جارية',
+            'name_en' => 'حسابات جارية',
+            'parent_id' => null,
+            'status' => 1,
+        ]);
+        $typeBankAccount = TypeBankAccount::create([
+            'branch_id' => $branch->id,
+            'name_ar' => 'حسابات ودائع وشهادات',
+            'name_en' => 'حسابات ودائع وشهادات',
+            'parent_id' => null,
+            'status' => 1,
+        ]);
+
+        $children = [
+            'child1' => [
+                'branch_id' => $branch->id,
+                'name_ar' => 'حسابات جارية دائنة',
+                'name_en' => 'حسابات جارية دائنة',
+                'parent_id' => $typeBankAccount->id,
+                'status' => 1,
+            ],
+            'child2' => [
+                'branch_id' => $branch->id,
+                'name_ar' => 'حسابات جارية مدينة',
+                'name_en' => 'حسابات جارية مدينة',
+                'parent_id' => $typeBankAccount->id,
+                'status' => 1,
+            ],
+        ];
+        foreach ($children as $child) {
+            TypeBankAccount::create($child);
+        }
+    }
+
+    public function deleted(Branch $branch)
+    {
+        TypeBankAccount::where('branch_id', $branch->id)->delete();
+    }
+}
